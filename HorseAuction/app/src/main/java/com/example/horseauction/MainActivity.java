25cadb060385fa998eb2341bbc946a1c452104dd
Camryn Bickerstaff <49502124+camrynbickerstaff@users.noreply.github.com>
2020-01-31 12:51:08 -0500

    Layout
    Edited layout and started MainActivity code

--- HorseAuction/app/src/main/java/com/example/horseauction/MainActivity.java
+++ HorseAuction/app/src/main/java/com/example/horseauction/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.horseauction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView abbypic;
+    ImageView rascalpic;
+    ImageView bothpic;
+    Button abby;
+    Button rascal;
+    Button both;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        abbypic = findViewById(R.id.abbypic);
+        abby = findViewById(R.id.abby);
+        rascalpic = findViewById(R.id.rascalpic);
+        rascal = findViewById(R.id.rascal);
+        bothpic = findViewById(R.id.bothpic);
+        both = findViewById(R.id.both);
+
+        abby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abbypic.setImageIcon(@mipmap/abbypic);
+            }
+
+        });
     }
 }
