fe6986e9209c0c9afb136672cc5f7253ee2eb7cf
Camryn M. Bickerstaff <camryn@belowaverage.org>
2020-02-07 17:54:13 -0500

    Tried to figure out blinking
    Blinking

--- SeeNSay/app/src/main/java/com/example/seensay/MainActivity.java
+++ SeeNSay/app/src/main/java/com/example/seensay/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.seensay;
 
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
     ImageView cow13;
     ImageView cow14;
     ImageView cow2pane;
+    ImageView cow4pane;
+    ImageView cow5pane;
+    ImageView cow6pane;
+    ImageView cow7pane;
+    ImageView cow8pane;
+    ImageView cow9pane;
+    ImageView cow10pane;
+    ImageView cow11pane;
+    ImageView cow12pane;
+    ImageView cow13pane;
+    ImageView cow14pane;
+    ImageView[] cows;
+
+
+
+
     Button button1;
 
     @Override
@@ -43,16 +62,59 @@ public class MainActivity extends AppCompatActivity {
         cow12 = findViewById(R.id.cow12);
         cow13 = findViewById(R.id.cow13);
         cow14 = findViewById(R.id.cow14);
+
         button1 = findViewById(R.id.button1);
+
         cow2pane = findViewById(R.id.cow2pane);
+        cow4pane = findViewById(R.id.cow4pane);
+        cow5pane = findViewById(R.id.cow5pane);
+        cow6pane = findViewById(R.id.cow6pane);
+        cow7pane = findViewById(R.id.cow7pane);
+        cow8pane = findViewById(R.id.cow8pane);
+        cow9pane = findViewById(R.id.cow9pane);
+        cow10pane = findViewById(R.id.cow10pane);
+        cow11pane = findViewById(R.id.cow11pane);
+        cow12pane = findViewById(R.id.cow12pane);
+        cow13pane = findViewById(R.id.cow13pane);
+        cow14pane = findViewById(R.id.cow14pane);
+
 
         button1.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-                cow2pane.setBackgroundColor(-65281);
-                cow2pane.getBackground().setAlpha(50);
+
+                cows = new ImageView[12];
+                cows[0] = cow2pane;
+                cows[1] = cow4pane;
+                cows[2] = cow5pane;
+                cows[3] = cow6pane;
+                cows[4] = cow7pane;
+                cows[5] = cow8pane;
+                cows[6] = cow9pane;
+                cows[7] = cow10pane;
+                cows[8] = cow11pane;
+                cows[9] = cow12pane;
+                cows[10] = cow13pane;
+                cows[11] = cow14pane;
+                int rando1 = 0;
+                int rando2 = 0;
+                for(int i = 0; i < 10; i++)
+                {
+                    rando1 = (int)(Math.random()*(12));
+                }
+
             }
         });
     }
+
+    void insideLoop(int rando1, int rando2)
+    {
+        cows[rando1].setBackgroundColor(-65281);
+        cows[rando1].getBackground().setAlpha(50);
+        rando2 = rando1;
+        cows[rando2].getBackground().setAlpha(0);
+        android.os.SystemClock.sleep(1000);
+    }
+
 }
